b5913ba439e94ca3c8334ab247876b81138abb5e
Nicolas DUMINIL <nicolas.duminil@simplex-software.fr>
2021-10-14 11:24:41 +0200

    Have migrate from AWS to minikube

--- demo-infrastructure/src/main/resources/scripts/deploy-k8s.sh
+++ demo-infrastructure/src/main/resources/scripts/deploy-k8s.sh
@@ -1,4 +1,8 @@
 #
+# Start minikube in insecure mode
+#
+minikube start --network-plugin=cni --enable-default-cni --bootstrapper=kubeadm --insecure-registry=172.19.0.2
+#
 # Deploy the k8s services
 #
 kubectl apply -f ./src/main/resources/k8s/demo-postgres-service.yaml,./src/main/resources/k8s/demo-postgres-deployment.yaml
@@ -7,4 +11,5 @@ kubectl apply -f ./src/main/resources/k8s/demo-gateway-service.yaml,./src/main/r
 kubectl apply -f ./src/main/resources/k8s/demo-activemq-service.yaml,./src/main/resources/k8s/demo-activemq-deployment.yaml
 kubectl apply -f ./src/main/resources/k8s/demo-api-service.yaml,./src/main/resources/k8s/demo-api-deployment.yaml
 kubectl apply -f ./src/main/resources/k8s/demo-job-service.yaml,./src/main/resources/k8s/demo-job-deployment.yaml
+kubectl apply -f ./src/main/resources/k8s/demo-xa-service.yaml,./src/main/resources/k8s/demo-xa-deployment.yaml
 kubectl apply -f ./src/main/resources/k8s/demo-adminer-service.yaml,./src/main/resources/k8s/demo-adminer-deployment.yaml
